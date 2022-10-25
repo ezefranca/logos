3a04a8a742bfae101f9849cf15fb3c2fd87980d2
Ezequiel Santos <ext.ezequiel.santos@farfetch.com>
2022-10-25 09:48:16 +0100

    logos

--- images.py
+++ images.py
@@ -18,7 +18,7 @@ names_file = open(script_dir + 'names.txt', 'r')
 names = names_file.readlines()
 
 for i, (name, image) in enumerate(zip(names, images)):
-    image_name = re.sub('\s', '_', name.strip()) + '.png'
+    image_name = re.sub('\s', '', name.strip()) + '.png'
     image_path = images_dir + image_name.lower()
     print(f'\n [ downloading {i + 1} of {len(names)} ]')
     try:
